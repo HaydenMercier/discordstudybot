f8f2686317fee6213006c93742945b8ac99e40b6
UltraArcher <ultra.archerhm@gmail.com>
2024-07-25 21:13:14 +1000

    updated the bot, just have to finish the break function and add some extra functionality!

--- bot.py
+++ bot.py
@@ -25,3 +25,36 @@ async def start(ctx):
         return
     
     session.isActive = True
+    session.sessionStartTime = ctx.message.created_at.timestamp()
+    humanReadableTime = datetime.datetime.now(aest_tz).strftime("%H:%M:%S") 
+    break_reminder.start()
+    await ctx.send(f"New session started at {humanReadableTime}")
+
+@bot.command()
+async def end(ctx):
+    if not session.isActive:
+        await ctx.send("No session is active!")
+        return
+    
+    session.isActive = False
+    session.sessionEndTime = ctx.message.created_at.timestamp()
+    duration = session.sessionEndTime - session.sessionStartTime - session.breakTime - session.pausedTime
+    humanReadableDuration = str(datetime.timedelta(seconds=duration))
+    break_reminder.stop()
+    await ctx.send(f"Session ended after {humanReadableDuration}")
+
+@bot.command()
+async def pause(ctx):
+    if not session.isActive:
+        await ctx.send("No session is active!")
+        return
+    
+    if session.pausedTime == 0:
+        session.pausedTime = ctx.message.created_at.timestamp()
+        await ctx.send("Session paused!")
+    else:
+        session.pausedTime = ctx.message.created_at.timestamp() - session.pausedTime
+        await ctx.send("Session unpaused!")
+
+@tasks.loop(minutes=maxSessionTimeMinutes, count=2)
+async def break_reminder():
